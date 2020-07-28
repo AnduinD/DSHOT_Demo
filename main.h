1fd4c7781d77db48e311aaa440494bea2fa5fc53
AnduinD <56484670+AnduinD@users.noreply.github.com>
2020-07-28 17:29:41 +0800

    Update main.h

--- main.h
+++ main.h
@@ -12,37 +12,6 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-//Mylib
-//#include "laser.h"
-//#include "MicroSw.h"
-//#include "pid.h"
-//#include "can1.h"
-//#include "can2.h"
-#include "tim8.h"
-//#include "usart1.h"
-//#include "usart6.h"
-//#include "frictionwheel.h"
-//#include "algorithmOfCRC.h"
-//#include "SteeringEngine.h"
-//Task lib
-//#include "ZeroCheckTask.h"
-//#include "DataSendTask.h"
-//#include "DataReceivetask.h"
-//#include "ActionTask.h"
-//#include "GimbalTask.h"
-//#include "ShootTask.h"
-//#include "ChassisTask.h"
 
-//typedef struct
-//{
-//	  unsigned short MagOpen;
-//	  unsigned short MagClose;
-//	  unsigned short Pitch_init;
-//	  unsigned short Yaw_init;
-//}RobotPRM_Struct;
-
-//void InfantryidInit(void);
-void System_Init(void);
-void System_Configration(void);
 void delay_ms(unsigned int t);
 #endif
